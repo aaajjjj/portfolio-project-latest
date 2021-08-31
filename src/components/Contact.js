5a4a51219299489620beccb24da003fa9bf5ccca
ajalrc <ajalrc456@gmail.com>
2021-08-31 17:34:55 -0500

    form settings

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,40 +12,17 @@ const Contact=()=>{
   });
   const [result,setResult] = useState(null);
 
-  const handlesubmit = (e) => {
-  e.preventDefault();
-  axios
-  .post('/send',{ ...state })
-  .then(response =>{
-    setResult(response.data);
-    //console.log(`The data sent to backend is  ${response.data}`)
-    setState({
-      first_name: '',
-      last_name: '',
-      email: '',
-      subject: '',
-      message: '',
-    })
-    //console.log("I did  run.")
-  })
-  .catch(()=>{
-    setResult({
-      success:false,
-      message:'Something went wrong. Please Try again later.'
-      })
-    })
-  }
-   const OnInputChange = event =>{
-    const {name,value}=event.target;
-
-    setState({
-      ...state,
-      [name]: value
-    });
-  };
     
   return (
     <>
+    <form name="contact" netlify netlify-honeypot="bot-field" hidden>
+      <input type="text" name="first_name" />
+      <input type="text" name="last_name" />
+      <input type="email" name="email" />
+      <input type="text" name="subject" />
+      <textarea name="message"></textarea>
+    </form>
+
     <div className="form-container" id="contact">
       <h2 className="about_header">Contact me</h2>
       {result && (
@@ -53,13 +30,14 @@ const Contact=()=>{
         {result.message}
         </p>
         )}
-      <form onSubmit={handlesubmit} className="contact-form"> 
+      <form  className="contact-form" data-netlify="true" data-netlify-honeypot="bot-field">
+        <input type="hidden" name="form-name" value="contact" /> 
         <div className="row">
             <div className="col-25">
               <label>First Name</label>
             </div>
             <div className="col-75">
-                <input id="first_name" name="first_name" value={state.first_name} onChange={OnInputChange} type="text" placeholder=" Your first name"/><br/>
+                <input id="first_name" name="first_name" value={state.first_name} type="text" placeholder=" Your first name"/><br/>
             </div>
         </div>
        
@@ -68,7 +46,7 @@ const Contact=()=>{
               <label>Last Name</label>
             </div>
               <div className="col-75">
-                 <input id="last_name" name="last_name" value={state.last_name} onChange={OnInputChange} type="text" placeholder="Your last name" /><br/>
+                 <input id="last_name" name="last_name" value={state.last_name} type="text" placeholder="Your last name" /><br/>
             </div>
         </div>
 
@@ -77,7 +55,7 @@ const Contact=()=>{
               <label>Email address</label>
             </div>
               <div className="col-75">
-                  <input id="email" name="email" value={state.email} onChange={OnInputChange} type="email" placeholder="Your email address" /><br/>
+                  <input id="email" name="email" value={state.email}  type="email" placeholder="Your email address" /><br/>
             </div>
         </div>
 
@@ -86,7 +64,7 @@ const Contact=()=>{
               <label>Subject</label>
               </div>
               <div className="col-75">
-                  <input id="subject" name="subject" value={state.subject} onChange={OnInputChange} type="text" placeholder="Subject" /><br/>
+                  <input id="subject" name="subject" value={state.subject}  type="text" placeholder="Subject" /><br/>
             </div>
         </div>
         
@@ -95,7 +73,7 @@ const Contact=()=>{
               <label>Text Area</label>
             </div>
               <div className="col-75">
-                  <textarea id="message" name="message" value={state.message} onChange={OnInputChange} type="text" placeholder="Message" /><br/>
+                  <textarea id="message" name="message" value={state.message} type="text" placeholder="Message" /><br/>
             </div>
         </div>
 

154af22f9cd6457e2b865aa9cd9b01a2cbf6f7e8
ajalrc <ajalrc456@gmail.com>
2021-08-31 19:52:42 -0500

    form settings part 7

--- src/components/Research.js
+++ src/components/Research.js
@@ -48,7 +48,7 @@ export default function Research() {
       <CardActions>
         <Button size="large" color="primary">
           <FaResearchgate size={30}
-          onClick={() => window.open('https://drakeedu-my.sharepoint.com/:p:/g/personal/ajal_rc_drake_edu/Eec0eE7_dwRBilzwoyeEV3wB3-40qD13EbfpSAwoxSbA_Q?e=7mJ5q4')}
+          onClick={() => window.open('https://drakeedu-my.sharepoint.com/:p:/g/personal/ajal_rc_drake_edu/Eec0eE7_dwRBilzwoyeEV3wBi6uRMPSrSwjC8Gy7U6Kf3g?e=Wgrrsf')}
           />
         </Button>
       </CardActions> 

409fa6dd49e9e45ec88bfeab51dd7cd6af8f5bcb
ajalrc <ajalrc456@gmail.com>
2021-09-19 11:38:39 -0500

    made about changes

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,7 @@ export default function About () {
             My name is Ajal RC. I am a senior at Drake university with Computer Science and Mathematics as majors with a minor in Physics. 
             I am a self- taught developer who is constantly exploring and teaching myself the evergrowing fields of Computer Science. I have completed projects in languages like Python, Java, Javascript, HTML and CSS, and their popular frameworks.
             Similarly, I have used platforms like Docker, Heroku, MongoDB, Datadog ,and AWS in my projects and internships. 
-            My way of learning is "Learning by doing". If I need to learn a new technology or language, I work on a project that uses that it. It is a harder way
+            My way of learning is "Learning by doing". If I need to learn a new technology or language, I work on a project that uses it. It is a harder way
             way of learning, but that's what makes it more fun and memorable.
             I believe Learning is the best investment that I could ever make in my life.</div>        
         </div>

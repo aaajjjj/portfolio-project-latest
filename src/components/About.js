58b710b3c826e4e9920382c1582b9dafda712ce1
ajalrc <ajalrc456@gmail.com>
2022-04-18 02:47:11 -0500

    made changes in about

--- src/components/About.js
+++ src/components/About.js
@@ -7,12 +7,12 @@ export default function About () {
         <div className="row" id="about">
             <h2 className="about_header">About me</h2>
             <Picture className="column1" src={Ab_pic}/> 
-            <div className="column2">Hello, 
+            <div className="column2">
             Hello, My name is Ajal RC. I am a graduating senior at Drake University with Computer 
-            Science and Mathematics as majors with a minor in Physics. I am a self-taught
+            Science and Mathematics as majors and a minor in Physics. I am a self-taught
             developer who is constantly exploring and teaching myself the ever-growing fields of 
             Computer Science. I have completed projects in languages like Python, Java, Javascript, 
-            HTML and CSS, and their popular frameworks. Similarly, I have used platforms like Docker, Azure DevOps,
+            and their popular frameworks like Node.js, React, Express and Django. Similarly, I have used platforms like Docker, Azure DevOps,
             Heroku, MongoDB, Datadog, and AWS in my projects and internships. My way of learning is 
             "Learning by doing". If I need to learn a new technology or language, I work on a project 
             that uses it. It is a harder way of learning, but that's what makes it more fun and
